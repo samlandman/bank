827029723d5f79e62603b4985da8b891b48f32ed
sam landman <sam landman>
2020-08-11 15:22:24 +0100

    Rewrote testing order for printer_spec

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,18 +1,18 @@
 require 'printer'
 
-describe 'Printer' do
+describe 'Printer -' do
 
-  before(:each) do
-  end
+  describe 'Statement Method:' do
 
-  it 'prints an empty line when no statements' do
-    expect(Printer.statement).to eq(['date || credit || debit || balance'])
-  end
+    it 'prints an empty line when no statements' do
+      expect(Printer.statement).to eq(['date || credit || debit || balance'])
+    end
 
-  it 'returns a printed line when provided with a hash' do
-    input = [{ :date => '14/01/2012', :credit => 2, :debit => 0 }]
-    expect(Printer.statement(input)).to eq(['date || credit || debit || balance', '14/01/2012 || 2.00 ||  || 2.00'])
+    it 'returns a printed line when provided with a hash' do
+      input = [{ :date => '14/01/2012', :credit => 2, :debit => 0 }]
+      expect(Printer.statement(input)).to eq(['date || credit || debit || balance', '14/01/2012 || 2.00 ||  || 2.00'])
+    end
+
   end
   
-
 end
