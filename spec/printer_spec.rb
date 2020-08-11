81711a926d0628a37cbc7bc702121193080b2228
sam landman <sam landman>
2020-08-11 15:02:10 +0100

    printer returns a printed line when provided with a hash

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -9,4 +9,10 @@ describe 'Printer' do
     expect(Printer.statement).to eq(["date || credit || debit || balance"])
   end
 
+  it 'returns a printed line when provided with a hash' do
+    input = [{:date => "14/01/2012", :credit => 2, :debit => 0}]
+    expect(Printer.statement(input)).to eq(["date || credit || debit || balance","14/01/2012 || 2.00 ||  || 2.00"])
+  end
+  
+
 end
